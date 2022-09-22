0ebbc0cdbcf588319900187e2f898f1856b15885
evodka <lexaeva12@gmail.com>
2022-09-22 21:59:42 +0300

    Esimene kodutöö

---
+++ Homework1/Homework1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mis on sinu nimi?");
+            string name = Console.ReadLine();
+            if (name != null)
+            {
+                Console.WriteLine($"Tere {name}, soovin sulle kõike head.");
+            }
+            else
+            {
+                Console.WriteLine("Palun sisesta enda nime");
+            }
+        }
+    }
+}
