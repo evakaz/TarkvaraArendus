1e33e8afca8b3678acbf466591a5888691775008
eva <lexaeva12@gmail.com>
2022-09-22 21:45:15 +0300

    initial commit

---
+++ FirstApp/FirstApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace FirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "Tere";
+            bool yes = true;
+            //Console.WriteLine(word);
+            string myInpute = Console.ReadLine();
+            //Console.WriteLine("Konsooli kirjutati: " + myInpute);
+            if (word == "Tere")
+            {
+                Console.WriteLine("eskere");
+            }
+            if(yes == true)
+            {
+                Console.WriteLine("yeees");
+            }
+            if (myInpute != String.Empty)
+            {
+                Console.WriteLine("Huuraaa");
+            }
+            else 
+            {
+                Console.WriteLine("Gucci Gang");
+            }
+            Console.ReadKey();
+        }
+    }
+}
